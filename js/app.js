e836b3573693a323edef42bd3896950ff4365bd2
Isabel <ycnia_1403@hotmail.com>
2018-01-02 04:41:28 -0500

    Actualizando informacion

--- js/app.js
+++ js/app.js
@@ -1,73 +1,72 @@
-$(function () {
-    // vista splash
-    setTimeout(function () {
-        $('#splash').fadeOut();
-        $('#city').removeClass('hide');
-    }, 4000);
+// vista splash
+$(function() {
+  setTimeout(function() {
+    $('#splash').fadeOut();
+    $('#city').removeClass('hide');
+  }, 4000);
 });
 
-// funcionalidad vista 1
-$('a.btn.white').click(function () {
-    $('#dropdown1').toggleClass('hide');
+$('.button-collapse').sideNav();
+
+// funcionalidad de la vista 1
+$('a.btn.white').click(function() {
+  $('#dropdown1').toggleClass('hide');
 });
-// estilos contenedor de banderas
-$('#dropdown1').attr({
-    'style': 'width: 7rem; position: absolute; top: 10rem; left: 4.5rem; display: block; opacity: 1'
-});
+
+$('#dropdown1').attr({'style': 'width: 90px; position: absolute; top: 158px; left: 108px; display: block; opacity: 1'});
 // Habilitando boton deacuerdo al contenido
-$('#input_text').keyup(function () {
-    if ($('#input_text').val().length < 10) {
-        $('#next').addClass('disabled');
-        $('#next').removeClass('btn-next');
-    } else if ($('#input_text').val().length == 10) {
-        $('#next').removeClass('grey lighten-2 disabled');
-        $('#next').addClass('btn-next');
-    }
+$('#input_text').keyup(function() {
+  if ($('#input_text').val().length < 10) {
+    $('#next').addClass('disabled');
+    $('#next').removeClass('btn-next');
+  } else if ($('#input_text').val().length == 10) {
+    $('#next').removeClass('grey lighten-2 disabled');
+    $('#next').addClass('btn-next');
+  }
 });
-// funcionalidad para el código aleatorio
+// Funcionalidad del codigo aleatorio
 function rand_code(chars, lon) {
-    code = '';
-    for (x = 0; x < lon; x++) {
-        rand = Math.floor(Math.random() * chars.length);
-        code += chars.substr(rand, 1);
-    }
-    return code;
-
-characters = '0123456789';
+  code = '';
+  for (x = 0; x < lon; x++) {
+    rand = Math.floor(Math.random() * chars.length);
+    code += chars.substr(rand, 1);
+  }
+  return code;
+}
+caracteres = '0123456789';
 longitud = 3;
 // Mostrando codigo aleatorio
-$('#next').click(function () {
-    alert('Tu codigo LAB -' + rand_code(characters, longitud));
-    $('#next').attr('href', 'page2.html');
+$('#next').click(function() {
+  alert('Tu codigo LAB -' + rand_code(caracteres, longitud));
+  $('#next').attr('href', 'page2.html');
 });
 
-// funcionalidad pagina 2
-$('#code').keyup(function () {
-    if ($('#code').val().length < 3) {
-        $('#next2').addClass('disabled');
-        $('#next2').removeClass('btn-next');
-    } else if ($('#code').val().length == 3) {
-        $('#next2').removeClass('grey lighten-2 disabled');
-        $('#next2').addClass('btn-next');
-        $('#next2').attr('href', 'page3.html');
-    }
+// funcionalidad de la vista 2
+$('#code').keyup(function() {
+  if ($('#code').val().length < 3) {
+    $('#next2').addClass('disabled');
+    $('#next2').removeClass('btn-next');
+  } else if ($('#code').val().length == 3) {
+    $('#next2').removeClass('grey lighten-2 disabled');
+    $('#next2').addClass('btn-next');
+    $('#next2').attr('href', 'page3.html');
+  }
 });
 
-// funcionalidad pagina 3
-$('#First-name').keyup(function () {
-    if ($('#First-name').val().length < 3) {
-        $('#next3').addClass('disabled');
-        $('#next3').removeClass('btn-next');
-    } else if ($('#First-name').val().length == 3) {
-        $('#next3').removeClass('grey lighten-2 disabled');
-        $('#next3').addClass('btn-next');
-    }
+// funcionalidad de la vista 3
+$('#First-name').keyup(function() {
+  if ($('#First-name').val().length < 3) {
+    $('#next3').addClass('disabled');
+    $('#next3').removeClass('btn-next');
+  } else if ($('#First-name').val().length == 3) {
+    $('#next3').removeClass('grey lighten-2 disabled');
+    $('#next3').addClass('btn-next');
+  }
 });
-
-$('#test5').click(function () {
-    if ($('input[type="checkbox"]:checked+label:before')) {
-        $('#next3').removeClass('grey lighten-2 disabled');
-        $('#next3').addClass('btn-next');
-        $('#next3').attr('href', 'page4.html');
-    }
+$('#test5').click(function() {
+  if ($('input[type="checkbox"]:checked+label:before')) {
+    $('#next3').removeClass('grey lighten-2 disabled');
+    $('#next3').addClass('btn-next');
+    $('#next3').attr('href', 'page4.html');
+  }
 });
